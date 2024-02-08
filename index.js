45c1cbc021dd74994a4a61c95c01b75f36f3cd8e
Nitesh Jat <113127294+NITESH-JAT@users.noreply.github.com>
2024-02-08 23:54:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let swiper = new Swiper(".slide-content", {
     loop: true,
     centeredSlides: true,
     grabCursor: true,
+    autoplay: {
+        delay: 5000, // Adjust delay in milliseconds (e.g., 5000 for 5 seconds)
+        disableOnInteraction: false, // Enable navigation even if user interacts with the slider
+    },
     navigation: {
       prevEl: ".swiper-button-prev",
       nextEl: ".swiper-button-next",
