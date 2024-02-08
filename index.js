9a9d37c133b3eb75cb27732556f8ee8b3b1a4e07
Nitesh Jat <113127294+NITESH-JAT@users.noreply.github.com>
2024-02-08 23:01:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let swiper = new Swiper(".slide-content", {
     centeredSlides: true,
     grabCursor: true,
     navigation: {
-      prevEl: ".swiper-button-next",
-      nextEl: ".swiper-button-prev",
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
     },
     breakpoints: {
       0: {
@@ -21,25 +21,7 @@ let swiper = new Swiper(".slide-content", {
     },
   });
 
-  swiper.on('transitionStart', function () {
-    document.querySelector('.slide-content').classList.add('swiper-in-transition');
-  });
 
-  swiper.on('transitionEnd', function () {
-    document.querySelector('.slide-content').classList.remove('swiper-in-transition');
-  });
 
-  // Add hover effect to the center card
-  swiper.on('transitionEnd', function () {
-    document.querySelectorAll('.swiper-slide').forEach((card) => {
-      card.classList.remove('hover-effect');
-    });
-
-    let centerCard = document.querySelector('.swiper-slide-active');
-    centerCard.classList.add('hover-effect');
-  });
-
-  // Initial hover effect on the first center card
-  document.querySelector('.swiper-slide-active').classList.add('hover-effect');
 
 
